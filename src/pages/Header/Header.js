b8e7bdabd5f3be15151c74ec6146130e401fe615
Markin Dessanti <markindessanti@gmail.com>
2020-06-30 13:44:30 -0300

    Inclusão do Reactstrap e estilização do header

--- src/pages/Header/Header.js
+++ src/pages/Header/Header.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+	Collapse,
+	Navbar,
+	NavbarToggler,
+	NavbarBrand,
+	Nav
+} from 'reactstrap';
+import './header.css';
 
-class Header extends React.Component {
-	render() {
-		return (
-			<footer>
-				<h1>Cabeçalho</h1>
-			</footer>
-		);
-	}
+const Header = (props) => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const toggle = () => setIsOpen(!isOpen);
+
+	return (
+		<div>
+			<Navbar color="warning" dark expand="md">
+				<NavbarBrand href="/">
+					<img className="logo" src="../../favicon.ico" alt="logo" />
+					<span className="logo-text">Gama Pokestore</span>
+				</NavbarBrand>
+				<NavbarToggler onClick={toggle} />
+				<Collapse isOpen={isOpen} navbar>
+					<Nav className="mr-auto" navbar>
+					</Nav>
+					<form className="form-inline my-2 my-lg-0">
+						<input id="inputPesquisar" className="form-control mr-sm-2" type="search" placeholder="Qual Pokémon você está procurando?" aria-label="Search"></input>
+						<button className="btn btn-outline-light my-2 my-sm-0" type="submit">Pesquisar</button>
+					</form>
+				</Collapse>
+			</Navbar>
+		</div>
+	);
 }
 
 export default Header;
