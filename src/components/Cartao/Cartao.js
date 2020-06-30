49e5272486b4bba5a6041563db9d3a1f972433a2
Markin Dessanti <markindessanti@gmail.com>
2020-06-30 17:58:23 -0300

    Alterado o texto e o estilo do botão dos cartões

--- src/components/Cartao/Cartao.js
+++ src/components/Cartao/Cartao.js
@@ -15,7 +15,9 @@ const Cartao = (props) => {
 				<CardTitle>Card title</CardTitle>
 				<CardSubtitle>Card subtitle</CardSubtitle>
 				<CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-				<Button>Button</Button>
+				<div className="div-card-button">
+					<Button className="card-button">Enviar para o carrinho</Button>
+				</div>
 			</CardBody>
 		</Card>
 	);
