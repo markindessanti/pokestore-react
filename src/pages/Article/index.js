f638bfd0c8f1c466874d5b3d35a29de520193531
Markin Dessanti <markindessanti@gmail.com>
2020-07-06 04:45:20 -0300

    Criados os botões de navegação Avançar e Voltar

--- src/pages/Article/index.js
+++ src/pages/Article/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Button } from 'reactstrap';
 import Cartao from '../../components/Cartao/Cartao';
 import './article.css';
 
@@ -11,27 +12,27 @@ const initListaInfo = {
 };
 
 function Article() {
+	const [urlFetch, setUrlFetch] = useState('https://pokeapi.co/api/v2/pokemon');
 	const [dados, setDados] = useState(null);
 	const [listaInfo, setListaInfo] = useState(initListaInfo);
 	const [dadosFinal, setDadosFinal] = useState(null);
-	const listaEmptCards = [0, 1, 2, 3]
-
-	async function getDados() {
-		const response = await fetch('https://pokeapi.co/api/v2/pokemon');
-		const json = await response.json();
-		setDados(json.results);
-	}
+	const listaEmptCards = [0, 1, 2, 3];
 
 	useEffect(() => {
+		async function getDados() {
+			const response = await fetch(urlFetch);
+			const json = await response.json();
+			setDados(json);
+		}
 		getDados();
-		const carrinho = {status: false, mensagem: 'Você ainda não selecionou nenhum pokémon para compra', lista:[]};
+		const carrinho = { status: false, mensagem: 'Você ainda não selecionou nenhum pokémon para compra', lista: [] };
 		localStorage.setItem('carrinho', JSON.stringify(carrinho));
-	}, [])
+	}, [urlFetch])
 
 	useEffect(() => {
 		let lista = [];
 		if (dados) {
-			dados.forEach(async (item) => {
+			dados.results.forEach(async (item) => {
 				const response = await fetch(item.url);
 				const json = await response.json();
 				const result = {
@@ -40,10 +41,12 @@ function Article() {
 					altura: (parseInt(json.height) * 10).toString() + ' cm',
 					peso: ((parseInt(json.weight) * 100) / 1000).toString().replace('.', ',') + ' kg',
 					sprite: `https://pokeres.bastionbot.org/images/pokemon/${json.id}.png`,
-					preco: (((parseInt(json.height) * 2) + (parseInt(json.weight) / 4)) * 2).toFixed(2).toString().replace('.', ',')
+					preco: (((parseInt(json.height) * 2) + (parseInt(json.weight) / 4)) * 2).toFixed(2).toString().replace('.', ','),
+					previous: dados.previous,
+					next: dados.next
 				}
 
-				lista.push(result)
+				lista.push(result);
 
 				if (lista.length === 20) {
 					setListaInfo(lista);
@@ -73,29 +76,79 @@ function Article() {
 	if (dadosFinal) {
 		const listaFinal = JSON.parse(dadosFinal)
 		return (
-			<main>
-				{
-					listaFinal.map((item) => {
-						return (
-							<Cartao key={item.id}
-								imagem={item.sprite}
-								nome={item.nome}
-								altura={item.altura}
-								peso={item.peso}
-								descricao={item.descricao}
-								preco={item.preco} />
-						)
-					})
-				}
+			<React.Fragment>
+				<div className="container navigation-top">
+					{
+						(
+							() => {
+								if (dados.previous) {
+									return(
+										<Button className="btn-voltar" color="info" onClick={() => setUrlFetch(dados.previous)}>Voltar</Button>
+									)
+								}
+							}
+						)()
+					}
+					{
+						(
+							() => {
+								if (dados.next) {
+									return(
+										<Button className="btn-avancar" color="info" onClick={() => setUrlFetch(dados.next)}>Avançar</Button>
+									)
+								}
+							}
+						)()
+					}
+				</div>
+				<main>
+					{
+						listaFinal.map((item) => {
+							return (
+								<Cartao key={item.id}
+									imagem={item.sprite}
+									nome={item.nome}
+									altura={item.altura}
+									peso={item.peso}
+									descricao={item.descricao}
+									preco={item.preco} />
+							)
+						})
+					}
 
-				{
-					listaEmptCards.map((item) => {
-						return (
-							<Cartao classe='card-hidden' key={item} imagem="" nome="" altura="" peso="" descricao="" preco="" />
-						)
-					})
-				}
-			</main>
+					{
+						listaEmptCards.map((item) => {
+							return (
+								<Cartao classe='card-hidden' key={item} imagem="" nome="" altura="" peso="" descricao="" preco="" />
+							)
+						})
+					}
+				</main>
+				<div className="container navigation-bottom">
+					{
+						(
+							() => {
+								if (dados.previous) {
+									return(
+										<Button className="btn-voltar" color="info" onClick={() => setUrlFetch(dados.previous)}>Voltar</Button>
+									)
+								}
+							}
+						)()
+					}
+					{
+						(
+							() => {
+								if (dados.next) {
+									return(
+										<Button className="btn-avancar" color="info" onClick={() => setUrlFetch(dados.next)}>Avançar</Button>
+									)
+								}
+							}
+						)()
+					}
+				</div>
+			</React.Fragment>
 		)
 	} else {
 		return (
