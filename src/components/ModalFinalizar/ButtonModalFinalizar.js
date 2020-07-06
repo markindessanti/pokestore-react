60af8d0a8bd6e492a6fc29ce1ff5c4963003a7fb
Markin Dessanti <markindessanti@gmail.com>
2020-07-05 23:44:49 -0300

    Modal Finalizar Compra concluído. Fata apagar compras efetuadas

--- src/components/ModalFinalizar/ButtonModalFinalizar.js
+++ src/components/ModalFinalizar/ButtonModalFinalizar.js
@@ -5,13 +5,16 @@ import "./modalFinalizar.css";
 
 const ButtonModalFinalizar = (props) => {
 	const [modal, setModal] = useState(false);
-
 	const toggle = () => setModal(!modal);
+
+	const carrinho = JSON.parse(localStorage.getItem('carrinho'));
+	
+	const statusCarrinho = carrinho.status;
 
 	return (
 		<React.Fragment>
 			<Button color="primary" className="btn" onClick={toggle}>{props.buttonLabel}</Button>
-			<ModalFinalizar buttonLabel="Fechar" className="modal-finalizar" modal={modal} toggle={toggle} />
+			<ModalFinalizar buttonLabel="Fechar" status={statusCarrinho} className="modal-finalizar" modal={modal} toggle={toggle} />
 		</React.Fragment>
 	);
 }
--- src/components/ModalFinalizar/index.js
+++ src/components/ModalFinalizar/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import './modalFinalizar.css';
+import compraOK from '../../assets/img/compraFinalizada.png';
+import compraFalha from '../../assets/img/compraFalha.png';
 
 const ModalFinalizar = (props) => {
 	const {
 		buttonLabel,
 		className,
 		modal,
-		toggle
+		toggle,
+		status
 	} = props;
 
 	return (
@@ -15,7 +18,27 @@ const ModalFinalizar = (props) => {
 			<Modal isOpen={modal} toggle={toggle} className={className}>
 				<ModalHeader cssModule={{ 'modal-title': 'w-100 text-center' }}>POKÉSTORE</ModalHeader>
 				<ModalBody>
-					Obrigado por comprar conosco
+					{
+						(
+							() => {
+								if (status) {
+									return (
+										<React.Fragment>
+											<img className="img-finalizar" src={compraOK} alt="Compra OK - Ash feliz!!!" />
+											<p>Obrigado por comprar na Gama Pokéstore!!!</p>
+										</React.Fragment>
+									)
+								} else {
+									return (
+										<React.Fragment>
+											<img className="img-finalizar" src={compraFalha} alt="Compra não concretizada - Pikachu triste..." />
+											<p>Você precisa selecionar algum pokémon antes de finalizar a compra...</p>
+										</React.Fragment>
+									)
+								}
+							}
+						)()
+					}
 				</ModalBody>
 				<ModalFooter>
 					<Button className="btn btn-danger" onClick={toggle}>{buttonLabel}</Button>
