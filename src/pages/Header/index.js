254ca5105f7d01ee4afcd4d2abfa81be4356054a
Markin Dessanti <markindessanti@gmail.com>
2020-07-07 16:51:58 -0300

    Implementada a manutenção do carrinho de compras ao voltar à página inicial (criado botão Home)

--- src/pages/Header/index.js
+++ src/pages/Header/index.js
@@ -35,13 +35,24 @@ const Header = (props) => {
 		<Container>
 			<header>
 				<Navbar color="warning" dark expand="md">
-					<NavbarBrand className="div-brand" href="/">
+					<NavbarBrand className="div-brand" onClick={
+						() => {
+							setTrocaDePagina(true);
+							setUrlFetch('https://pokeapi.co/api/v2/pokemon');
+						}
+					}>
 						<img className="logo" src="../../favicon.ico" alt="logo" />
 						<span className="logo-text">GAMA POKÉSTORE</span>
 					</NavbarBrand>
 					<NavbarToggler onClick={toggle} />
 					<Collapse isOpen={isOpen} navbar>
 						<Nav className="mr-auto" navbar>
+							<button className="btn btn-outline-light my-2 my-sm-0" onClick={
+								() => {
+									setTrocaDePagina(true);
+									setUrlFetch('https://pokeapi.co/api/v2/pokemon');
+								}
+							}>Home</button>
 						</Nav>
 						<div className="form-inline my-2 my-lg-0">
 							<input id="inputPesquisar" className="form-control mr-sm-2" type="search" placeholder="Qual Pokémon você quer?" aria-label="Search" value={usuario} onChange={e => setUsuario(e.target.value)}></input>
